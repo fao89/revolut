71ee810dae03bb5f3d3fe877b6a877a1d32d3867
Fabrício Aguiar <fabricio-aguiar@pop-os.localdomain>
2019-03-23 19:46:00 -0300

    test for adding two currencies

--- revolut/tests/test_input.py
+++ revolut/tests/test_input.py
@@ -22,4 +22,43 @@ def test_add_one_currency():
         }
     },
     }
+
+def test_add_two_currencies():
+    currency = CurrencyJson([
+        {
+        "country": "UK",
+        "city": "London",
+        "currency": "GBP",
+        "amount": 12.2
+        },
+        {
+        "country": "FR",
+        "city": "Paris",
+        "currency": "EUR",
+        "amount": 20
+        },
+    ])
+
+    currency.parse()
+    assert currency.output == {
+        "GBP": {
+        "UK": {
+        "London": [
+            {
+            "amount": 12.2
+            }
+        ]
+        }
+        },
+        "EUR": {
+        "FR": {
+        "Paris": [
+            {
+            "amount": 20
+            }
+        ]
+        }
+        },
+    }
+
     
