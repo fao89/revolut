135bb65e544952e898ace765688d4aba35ece23c
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2019-03-24 05:20:51 -0300

    adding simple login

--- revolut/app.py
+++ revolut/app.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask
-
+from flask_simplelogin import SimpleLogin
 from revolut.rest import parser
 
 
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
+    SimpleLogin(app)
     app.register_blueprint(parser.blueprint)
     return app
--- revolut/rest/parser.py
+++ revolut/rest/parser.py
@@ -1,11 +1,13 @@
 import json
 from flask import Blueprint, request, jsonify
+from flask_simplelogin import login_required
 
 from revolut.use_cases.json_parser import ParseJson
 
 blueprint = Blueprint('json_parser', __name__)
 
 @blueprint.route('/parse', methods=['POST'])
+@login_required(basic=True)
 def json_parser():
     for arg in request.args.keys():
         if arg.startswith('nest_'):
