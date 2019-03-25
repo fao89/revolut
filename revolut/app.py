1dfa0cdfad0631e24e7c0507e51cc6af4836a531
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2019-03-25 01:08:46 -0300

    updating app dependencies

--- revolut/app.py
+++ revolut/app.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask
+from flask_migrate import Migrate
 from flask_simplelogin import SimpleLogin
-from revolut.rest import parser
+from revolut.rest import parser, user, login
+from revolut.models import configure as config_db
+from revolut.serializers import configure as config_ma
 
 SECRET = os.environ.get("SECRET_KEY")
 
@@ -9,6 +12,12 @@ SECRET = os.environ.get("SECRET_KEY")
 def create_app(secret=SECRET):
     app = Flask(__name__)
     app.config["SECRET_KEY"] = secret
-    SimpleLogin(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/revolut.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    config_db(app)
+    config_ma(app)
+    Migrate(app, app.db)
+    login.configure(app)
     app.register_blueprint(parser.blueprint)
+    app.register_blueprint(user.bp_user)
     return app
