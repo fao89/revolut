8d789baf18aedc717e445fa9f64960886ab643a8
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2019-03-31 19:41:12 -0300

    refac initial_postgres_setup

--- initial_postgres_setup.py
+++ initial_postgres_setup.py
@@ -5,13 +5,17 @@ import sqlalchemy_utils
 
 load_dotenv()
 
-conn_str = "postgresql+psycopg2://{}:{}@{}:5433/{}".format(
-    os.environ.get('POSTGRES_USER'),
-    os.environ.get('POSTGRES_PASS'),
-    os.environ.get('POSTGRES_HOST'),
-    os.environ.get('POSTGRES_DB')
-)
+def create_database():
+    conn_str = "postgresql+psycopg2://{}:{}@{}:5433/{}".format(
+        os.environ.get('POSTGRES_USER'),
+        os.environ.get('POSTGRES_PASS'),
+        os.environ.get('POSTGRES_HOST'),
+        os.environ.get('POSTGRES_DB')
+    )
 
-engine = sqlalchemy.create_engine(conn_str)
-sqlalchemy_utils.create_database(engine.url)
+    engine = sqlalchemy.create_engine(conn_str)
+    sqlalchemy_utils.create_database(engine.url)
 
+
+if __name__ == "__main__":
+    create_database()
