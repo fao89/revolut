11a0cd6ff3f150e9fc338924d9ee5df24028d344
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2019-04-04 03:54:59 -0300

    starting sep

--- revolut/rest/parser.py
+++ revolut/rest/parser.py
@@ -9,14 +9,15 @@ blueprint = Blueprint('json_parser', __name__)
 @blueprint.route('/parse', methods=['POST'])
 @login_required(basic=True)
 def json_parser():
-    query_string = request.query_string.decode("utf-8") 
+    query_string = request.query_string.decode("utf-8")
+    sep = '' 
     if '&' in query_string:
         sep = '&'
     elif ',' in query_string:
         sep = ','
     elif ';' in query_string:
         sep = ';'
-
+    
     nest_levels = query_string.replace("nest=", "").split(sep)
 
     convert = ParseJson(request.json, nest_levels)
