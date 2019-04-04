508a9d88606c461133084a14ffc5d198c336b4be
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2019-04-03 23:49:42 -0300

    improving sql queries

--- SQL_Challenge/one.sql
+++ SQL_Challenge/one.sql
@@ -1,29 +1,26 @@
-SELECT Sum(user_amount_in_gbp.amount) AS total_spent_gbp,
-       user_id
-FROM  (SELECT ( rate * Sum(amount) ) AS amount,
-              user_id
-       FROM  (SELECT exchange_rates.rate,
-                     exchange_rates.from_currency
-              FROM   exchange_rates
-                     INNER JOIN (SELECT Max(ts) AS ts,
-                                        from_currency
-                                 FROM   exchange_rates
-                                 WHERE  to_currency = 'GBP'
-                                 GROUP  BY from_currency) AS max_currencies
-                             ON exchange_rates.ts = max_currencies.ts
-                                AND exchange_rates.from_currency =
-                                    max_currencies.from_currency
-              WHERE  to_currency = 'GBP'
-              UNION
-              SELECT 1     AS rate,
-                     'GBP' AS from_currency) AS rate_per_currency
-             INNER JOIN (SELECT Sum(amount) AS amount,
-                                user_id,
-                                currency
-                         FROM   transactions
-                         GROUP  BY user_id,
-                                   currency) AS user_amount_per_currency
-                     ON user_amount_per_currency.currency = rate_per_currency.from_currency
-       GROUP  BY rate_per_currency.rate,
-                 user_amount_per_currency.user_id) AS user_amount_in_gbp
-GROUP  BY user_amount_in_gbp.user_id; 
+WITH exchange_rate_with_max
+     AS (SELECT *,
+                Max(ts)
+                  OVER (
+                    partition BY from_currency) max_ts
+         FROM   exchange_rates
+         WHERE  to_currency = 'GBP'),
+     max_exchange_rate
+     AS (SELECT *
+         FROM   exchange_rate_with_max
+         WHERE  ts = max_ts
+         UNION
+         SELECT Now() AS ts,
+                'GBP' AS from_currency,
+                'GBP' AS to_currency,
+                1     AS rate,
+                Now() AS max_ts),
+     converted_amount_per_transaction
+     AS (SELECT Sum(rate * amount) AS total_spent_gbp,
+                user_id
+         FROM   transactions,
+                max_exchange_rate
+         WHERE  currency = from_currency
+         GROUP  BY user_id)
+SELECT *
+FROM   converted_amount_per_transaction 
--- SQL_Challenge/two_and_three.sql
+++ SQL_Challenge/two_and_three.sql
@@ -1,38 +1,37 @@
-SELECT Sum(user_id_amount_and_rate_per_currency.rate * user_id_amount_and_rate_per_currency.amount)AS user_amount_in_gbp,
-       user_id_amount_and_rate_per_currency.user_id
-FROM  (SELECT user_id_amount_per_ts.currency,
-              user_id_amount_per_ts.user_id,
-              user_id_amount_per_ts.amount,
-              exchange_rates.rate
-       FROM  (SELECT Max(exchange_rates_joined_transactions.e_ts) AS ts,
-                     exchange_rates_joined_transactions.currency,
-                     exchange_rates_joined_transactions.user_id,
-                     exchange_rates_joined_transactions.amount
-              FROM   (SELECT exchange_rates.ts AS e_ts,
-                             transactions.ts   AS t_ts,
-                             exchange_rates.rate,
-                             transactions.user_id,
-                             transactions.currency,
-                             transactions.amount
-                      FROM   exchange_rates
-                             LEFT OUTER JOIN transactions
-                                          ON exchange_rates.from_currency =
-                                             transactions.currency
-                      WHERE  exchange_rates.ts <= transactions.ts
-                             AND to_currency = 'GBP') AS exchange_rates_joined_transactions
-              GROUP  BY t_ts,
-                        currency,
-                        user_id,
-                        amount) AS user_id_amount_per_ts
-             INNER JOIN exchange_rates
-                     ON user_id_amount_per_ts.ts = exchange_rates.ts
-                        AND user_id_amount_per_ts.currency = exchange_rates.from_currency
-       WHERE  to_currency = 'GBP'
-       UNION
-       SELECT currency,
-              user_id,
-              amount,
-              1 AS rate
-       FROM   transactions
-       WHERE  currency = 'GBP') AS user_id_amount_and_rate_per_currency
-GROUP  BY user_id_amount_and_rate_per_currency.user_id; 
+WITH exchange_rate_with_max AS
+  (SELECT Max(exchange_rates.ts) FILTER (
+                                         WHERE exchange_rates.ts <= transactions.ts) AS ts,
+          from_currency AS currency,
+          user_id,
+          amount
+   FROM exchange_rates,
+        transactions
+   WHERE to_currency = 'GBP'
+     AND currency = from_currency
+   GROUP BY transactions.ts,
+            from_currency,
+            amount,
+            user_id),
+     max_exchange_rate AS
+  (SELECT exchange_rate_with_max.*,
+          rate
+   FROM exchange_rate_with_max,
+        exchange_rates
+   WHERE exchange_rate_with_max.ts = exchange_rates.ts
+     AND currency = from_currency
+     AND to_currency = 'GBP'
+   UNION SELECT ts,
+                currency,
+                user_id,
+                amount,
+                1 AS rate
+   FROM transactions
+   WHERE currency = 'GBP'),
+     converted_amount_per_transaction AS
+  (SELECT sum(rate * amount) AS total_spent_gbp,
+          user_id
+   FROM max_exchange_rate
+   GROUP BY user_id)
+SELECT *
+FROM converted_amount_per_transaction
+ORDER BY user_id
